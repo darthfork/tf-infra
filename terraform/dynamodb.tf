f7a114a59ae2792078fe71ddcf16e20d9fa5a219
Abhishek Rai <rai.abhishek90@gmail.com>
2021-07-17 20:04:43 -0700

    terraform fmt

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "tf-state-lock" {
-  name = "tf-state-lock"
-  hash_key = "LockID"
-  read_capacity = 20
+  name           = "tf-state-lock"
+  hash_key       = "LockID"
+  read_capacity  = 20
   write_capacity = 20
 
   attribute {
