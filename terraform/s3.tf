9d13fcfc2e3f953ae6a027efe35c5234dbfa2b79
Abhishek Rai <rai.abhishek90@gmail.com>
2021-07-22 11:55:29 -0700

    Add s3 public access block policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "tf-backend" {
-   bucket = "tf-backend-abhishek"
+  bucket = "tf-backend-abhishek"
 }
 
 resource "aws_s3_bucket_policy" "tf-backend-policy" {
@@ -9,25 +9,34 @@ resource "aws_s3_bucket_policy" "tf-backend-policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
+        Effect    = "Allow"
         Principal = "*"
-        Action = "s3:ListBucket",
-        Resource = aws_s3_bucket.tf-backend.arn
+        Action    = "s3:ListBucket",
+        Resource  = aws_s3_bucket.tf-backend.arn
       },
       {
-          Effect = "Allow",
-          Principal = {
-              "AWS": [
-                  "arn:aws:iam::${var.aws_account_number}:user/arai",
-                  "arn:aws:iam::${var.aws_account_number}:root"
-              ]
-          },
-          Action = [
-              "s3:GetObject",
-              "s3:PutObject"
-          ],
-          Resource = "${aws_s3_bucket.tf-backend.arn}/terraform/terraform.tfstate"
+        Effect = "Allow",
+        Principal = {
+          "AWS" : [
+            "arn:aws:iam::${var.aws_account_number}:user/arai",
+            "arn:aws:iam::${var.aws_account_number}:root"
+          ]
+        },
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = "${aws_s3_bucket.tf-backend.arn}/terraform/terraform.tfstate"
       }
     ]
   })
+}
+
+resource "aws_s3_bucket_public_access_block" "tf-backend-public-access-block" {
+  bucket = aws_s3_bucket.tf-backend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
