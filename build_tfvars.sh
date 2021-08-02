6bc0327a3837f48ba12500e9cdb284504adb144d
Abhishek Rai <rai.abhishek90@gmail.com>
2021-08-02 16:23:05 -0700

    terraform fmt
    Signed-off-by: Abhishek Rai <rai.abhishek90@gmail.com>

--- build_tfvars.sh
+++ build_tfvars.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cat << HEREDOC > terraform/terraform.tfvars
-aws_access_key="$AWS_ACCESS_KEY_ID"
-aws_secret_key="$AWS_SECRET_ACCESS_KEY"
-aws_account_number="$AWS_ACCOUNT_NUMBER"
+aws_access_key     = "$AWS_ACCESS_KEY_ID"
+aws_secret_key     = "$AWS_SECRET_ACCESS_KEY"
+aws_account_number = "$AWS_ACCOUNT_NUMBER"
 HEREDOC
