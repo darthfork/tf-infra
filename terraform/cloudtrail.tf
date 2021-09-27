05fefd081c27f0e10d86fa6ffed43e1cef78cfcb
Abhishek Rai <rai.abhishek90@gmail.com>
2021-09-27 14:23:30 -0700

    chore: terraform fmt
    Signed-off-by: Abhishek Rai <rai.abhishek90@gmail.com>

--- terraform/cloudtrail.tf
+++ terraform/cloudtrail.tf
@@ -1,6 +1,7 @@
 resource "aws_cloudtrail" "management" {
+  count                         = 1
   name                          = "tf-trail-management"
   s3_bucket_name                = aws_s3_bucket.tf-management-trail.id
-  s3_key_prefix                 = "prefix"
+  s3_key_prefix                 = "management"
   include_global_service_events = false
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -47,25 +47,25 @@ resource "aws_s3_bucket_policy" "tf-management-trail-policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid = "AWSCloudTrailAclCheck",
-        Effect    = "Allow",
-        Principal = {
-          Service = "cloudtrail.amazonaws.com"
-        },
-        Resource  = aws_s3_bucket.tf-management-trail.arn
-        Action    = "s3:GetBucketAcl"
-      },
-      {
-        Sid = "AWSCloudTrailWrite",
+        Sid    = "AWSCloudTrailAclCheck",
         Effect = "Allow",
         Principal = {
           Service = "cloudtrail.amazonaws.com"
         },
-        Action = "s3:PutObject",
-        Resource = "${aws_s3_bucket.tf-management-trail.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Resource = aws_s3_bucket.tf-management-trail.arn
+        Action   = "s3:GetBucketAcl"
+      },
+      {
+        Sid    = "AWSCloudTrailWrite",
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action   = "s3:PutObject",
+        Resource = "${aws_s3_bucket.tf-management-trail.arn}/management/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
         Condition = {
           StringEquals = {
-            "s3:x-amz-acl": "bucket-owner-full-control"
+            "s3:x-amz-acl" : "bucket-owner-full-control"
           }
         }
       }
