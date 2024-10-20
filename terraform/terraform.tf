f9a9503b49d1a7dc9f09d58b418ab718f75f937b
Abhishek Rai <rai.abhishek90@gmail.com>
2024-10-19 22:58:34 -0700

    chore: update terraform version to 1.9.8
    Signed-off-by: Abhishek Rai <rai.abhishek90@gmail.com>

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -5,7 +5,7 @@ terraform {
     dynamodb_table = "tf-state-lock"
     region         = "us-east-1"
   }
-  required_version = "~> 1.4.6"
+  required_version = "~> 1.9.8"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
