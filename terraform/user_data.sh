620f14b40018f9d0d495c1d50eb6fa1db8922001
Abhishek Rai <abhishek.rai@lacework.net>
2024-10-19 21:15:14 -0700

    feat: Switch to debian based AMIs (#35)
    * feat: Switch to debian based AMIs
    
    Signed-off-by: Abhishek Rai <rai.abhishek90@gmail.com>
    
    * fix instance name
    
    Signed-off-by: Abhishek Rai <rai.abhishek90@gmail.com>
    
    ---------
    
    Signed-off-by: Abhishek Rai <rai.abhishek90@gmail.com>

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-sudo dnf -y update
-sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf -y install docker-ce docker-ce-cli containerd.io python3-pip
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io python3-pip
